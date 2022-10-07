427a38f2a70f97e361aef8045fbf6bc88fad9e7b
Jose Vicente Campos <josevcm@gmail.com>
2022-10-07 20:54:35 +0200

    update libusb to 1.0.26

--- src/nfc-app/app-qt/src/main/cpp/main.cpp
+++ src/nfc-app/app-qt/src/main/cpp/main.cpp
@@ -520,7 +520,7 @@ int main(int argc, char *argv[])
 //   return startTest1(argc, argv);
 //   return startTest2(argc, argv);
 //   return startTest3(argc, argv);
-   return startTest4(argc, argv);
-//   return startApp(argc, argv);
+//   return startTest4(argc, argv);
+   return startApp(argc, argv);
 }
 
